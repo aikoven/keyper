a863b9236c19e84c838e7f5d1eb3f7fbe33798bc
Daniel Lytkin <gaelco.salmon@gmail.com>
2015-07-16 14:04:58 +0600

    add re-exports to `keyper` module

--- src/keyper.ts
+++ src/keyper.ts
@@ -1,1 +1,14 @@
 /// <reference path="../typings/tsd.d.ts" />
+
+export * from './collection';
+export * from './db';
+export * from './query';
+export * from './uniqueIndex';
+
+export {default as common} from './common';
+
+export * from './view';
+
+export module dataSource {
+    export * from './dataSource/restApi';
+}
